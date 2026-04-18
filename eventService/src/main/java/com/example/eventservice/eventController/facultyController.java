bc1170c55232f24edbfc8927f18389e37aaeb2ed
Praveenpk2112 <pkpraveen2kumar@gmail.com>
2026-04-18 11:35:01 +0530

    Update facultyController.java

--- eventService/src/main/java/com/example/eventservice/eventController/facultyController.java
+++ eventService/src/main/java/com/example/eventservice/eventController/facultyController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/faculty")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "*")
 public class facultyController {
 
     private eventService serv;
